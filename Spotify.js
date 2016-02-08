cd7b0c7404633508f6630e429e6eee5fd2b087e4
Marco Hyyryläinen <marco@wheresmar.co>
2016-02-08 12:59:13 +0100

    Added package and spotify file

---
+++ Spotify.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// Get the requires in order
+var http = require("http");
+var config = require('./config');
--- config_sample.js
+++ config_sample.js
@@ -1,5 +1,6 @@
 var config = {};
 
 config.musixmatchAPI = "";
+config.spotifyClientSecret = ""; // base64 encoded "client_id:client_secret"
 
 module.exports = config;
