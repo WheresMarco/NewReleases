f5ebd4bae6246fd91e24b470a510b4925c3185d3
Marco Hyyryläinen <marco@wheresmar.co>
2016-02-08 16:35:35 +0100

    Started stumbeling around with displaying content

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 "use strict";
 
+var http = require("http");
 var Albums = require("./Albums");
 var Spotify = require("./Spotify");
 
-Albums.getArtistID("hellogoodbye")
-  .then(function(data) {
-    var artistID = data.message.body.artist_list[0].artist.artist_id;
-    return Albums.getAlbums(artistID);
-  })
-  .then(function(data) {
-    console.log(data.message.body.album_list[0].album);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+// Create the webserver
+http.createServer(function(request, response) {
+  response.writeHeader(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*'});
+  response.end("A page here, please.");
+
+}).listen(3000, '127.0.0.1');
+
+// Console info
+console.log("Server running at http://127.0.0.1:3000");
+
+
+// Albums.getArtistID("hellogoodbye")
+//   .then(function(data) {
+//     var artistID = data.message.body.artist_list[0].artist.artist_id;
+//     return Albums.getAlbums(artistID);
+//   })
+//   .then(function(data) {
+//     console.log(data.message.body.album_list[0].album);
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
+
+// Spotify.getUsersArtists()
+//   .then(function(data) {
+//     console.log(data);
+//   });
